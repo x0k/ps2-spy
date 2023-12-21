703fe630bdfb3e781f3bc48d9af2852f9a61a93d
Krasilnikov Roman <x0kill@ya.ru>
2023-12-21 15:26:54 +0300

    Ping implementation

--- cmd/ps2-feed-ds/main.go
+++ cmd/ps2-feed-ds/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"os/signal"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -11,17 +13,41 @@ var (
 	discord_token string
 )
 
-const API_URL = "some_url"
-
 func init() {
 	flag.StringVar(&discord_token, "t", "", "Bot Token")
 	flag.Parse()
 }
 
 func main() {
-	dg, err := discordgo.New("Bot " + discord_token)
+	ds, err := discordgo.New("Bot " + discord_token)
 	if err != nil {
-		log.Fatalln("Error creating Discord session: ", err)
+		log.Fatalln("error creating Discord session: ", err)
+	}
+	ds.AddHandler(handleMessageCreate)
+
+	ds.Identify.Intents = discordgo.IntentGuildMessages
+
+	err = ds.Open()
+	if err != nil {
+		log.Fatalln("error opening connection: ", err)
 	}
 
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, os.Interrupt, os.Kill)
+	<-sc
+
+	ds.Close()
+}
+
+func handleMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "/ping" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "Pong!")
+		if err != nil {
+			log.Println("error sending message:", err)
+		}
+	}
 }
