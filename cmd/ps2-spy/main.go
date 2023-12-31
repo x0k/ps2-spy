ca8e076ff314175167433f3c48a49f6aee54a0bf
Krasilnikov Roman <x0kill@ya.ru>
2023-12-31 22:38:49 +0300

    Saerro loader

--- cmd/ps2-spy/main.go
+++ cmd/ps2-spy/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/x0k/ps2-spy/internal/lib/honu"
 	"github.com/x0k/ps2-spy/internal/lib/ps2alerts"
 	"github.com/x0k/ps2-spy/internal/lib/ps2live/population"
+	"github.com/x0k/ps2-spy/internal/lib/ps2live/saerro"
 	"github.com/x0k/ps2-spy/internal/lib/voidwell"
 	"github.com/x0k/ps2-spy/internal/ps2"
 	"github.com/x0k/ps2-spy/internal/ps2/loaders"
@@ -34,28 +35,33 @@ func main() {
 	fisuClient := fisu.NewClient("https://ps2.fisu.pw", httpClient)
 	voidWellClient := voidwell.NewClient("https://api.voidwell.com", httpClient)
 	populationClient := population.NewClient("https://agg.ps2.live", httpClient)
+	saerroClient := saerro.NewClient("https://saerro.ps2.live", httpClient)
 	ps2alertsClient := ps2alerts.NewClient("https://api.ps2alerts.com", httpClient)
 	censusClient := census2.NewClient("https://census.daybreakgames.com", "", httpClient)
 	honuClient.Start()
 	fisuClient.Start()
 	voidWellClient.Start()
 	populationClient.Start()
+	saerroClient.Start()
 	ps2alertsClient.Start()
 	defer honuClient.Stop()
 	defer fisuClient.Stop()
 	defer voidWellClient.Stop()
 	defer populationClient.Stop()
+	defer saerroClient.Stop()
 	defer ps2alertsClient.Stop()
 	worldsLoader := ps2.WithFallback(
 		"Worlds",
 		ps2.WithLoaded(loaders.NewPS2LiveWorldsPopulationLoader(populationClient)),
 		ps2.WithLoaded(loaders.NewHonuWorldsPopulationLoader(honuClient)),
+		ps2.WithLoaded(loaders.NewSaerroWorldsPopulationLoader(saerroClient)),
 		ps2.WithLoaded(loaders.NewFisuWorldsPopulationLoader(fisuClient)),
 		ps2.WithLoaded(loaders.NewVoidWellWorldsPopulationLoader(voidWellClient)),
 	)
 	worldLoader := ps2.WithKeyedFallback(
 		"World",
 		ps2.WithKeyedLoaded(loaders.NewHonuWorldPopulationLoader(honuClient)),
+		ps2.WithKeyedLoaded(loaders.NewSaerroWorldPopulationLoader(saerroClient)),
 		ps2.WithKeyedLoaded(loaders.NewVoidWellWorldPopulationLoader(voidWellClient)),
 	)
 	alertsLoader := ps2.WithFallback(
