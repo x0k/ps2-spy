af209de5ec694a51fb0d23e763c194ac4ce01b63
Krasilnikov Roman <x0kill@ya.ru>
2024-01-23 18:10:25 +0300

    Continent mention in logout message

--- internal/bot/render/character_logout.go
+++ internal/bot/render/character_logout.go
@@ -10,15 +10,17 @@ import (
 func RenderCharacterLogout(char ps2.Character) string {
 	if char.OutfitTag != "" {
 		return fmt.Sprintf(
-			"[%s] %s (%s) is now offline",
+			"[%s] %s (%s) is now offline (%s)",
 			char.OutfitTag,
 			char.Name,
 			factions.FactionNameById(char.FactionId),
+			ps2.WorldNameById(char.WorldId),
 		)
 	}
 	return fmt.Sprintf(
-		"%s (%s) is now offline",
+		"%s (%s) is now offline (%s)",
 		char.Name,
 		factions.FactionNameById(char.FactionId),
+		ps2.WorldNameById(char.WorldId),
 	)
 }
