9176d1b2a4b70efab005833da7fa156e724537a1
Krasilnikov Roman <x0kill@ya.ru>
2024-11-21 19:38:31 +0300

    Fix env variables names

--- internal/app/config.go
+++ internal/app/config.go
@@ -34,9 +34,9 @@ type HttpClientConfig struct {
 }
 
 type Config struct {
-	AppName           string `yaml:"app_name" env:"PS2_APP_NAME" env-default:"PS 2 Spy"`
-	StreamingEndpoint string `yaml:"streaming_endpoint" env:"PS2_STREAMING_ENDPOINT" env-default:"wss://push.planetside2.com/streaming"`
-	CensusServiceId   string `yaml:"census_service_id" env:"PS2_CENSUS_SERVICE_ID" env-required:"true"`
+	AppName           string `yaml:"app_name" env:"APP_NAME" env-default:"PS 2 Spy"`
+	StreamingEndpoint string `yaml:"streaming_endpoint" env:"STREAMING_ENDPOINT" env-default:"wss://push.planetside2.com/streaming"`
+	CensusServiceId   string `yaml:"census_service_id" env:"CENSUS_SERVICE_ID" env-required:"true"`
 
 	Logger     LoggerConfig     `yaml:"logger"`
 	Profiler   ProfilerConfig   `yaml:"profiler"`
